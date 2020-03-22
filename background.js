30309c8c5043be62f0fdf81ec1b364d6644ab286
Ben Edelstein <bedelstein12@gmail.com>
2020-03-22 15:56:29 -0400

    add call to re-run libraries before content script

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) { //wait for dom to be updated
   if(details.url.includes('twitter.com/search?q=%24')) {
     console.log(details.transitionType);
+    chrome.tabs.executeScript(null,{file:"jquery-3.4.1.min.js"}); // reload jquery first
+    chrome.tabs.executeScript(null,{file:"plotly-basic.min.js"});
+    chrome.tabs.executeScript(null,{file:"stocks.js"});
     chrome.tabs.executeScript(null,{file:"content.js"}); //re-run content script
   }
 });
