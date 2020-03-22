fb1a363f7c42a11834669ecbc2f22b4ca0e3bac0
Ben Edelstein <bedelstein12@gmail.com>
2020-03-22 15:14:36 -0400

    add comments

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) { //wait for dom to be updated
   if(details.url.includes('twitter.com/search?q=%24')) {
     console.log(details.transitionType);
-    chrome.tabs.executeScript(null,{file:"content.js"});
+    chrome.tabs.executeScript(null,{file:"content.js"}); //re-run content script
   }
 });
