0ada883a9d89eef54a9309085022ca1c422c0793
Ben Edelstein <bedelstein12@gmail.com>
2020-03-22 20:22:01 -0400

    moved message to be sent after api key check

--- popup.js
+++ popup.js
@@ -8,15 +8,15 @@ $(document).ready(function() {
       currentWindow: true
     }
     let apiKey = $('#keyInput').val().trim();
-    if(apiKey.length===16) {
+    if(apiKey.length===16) { // if key is valid
       chrome.storage.sync.set({apiKey: apiKey},function() {
         console.log('Set user api key to ' + apiKey);
-      })
+      });
+      chrome.tabs.query(params, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {message: "keyIn",key:apiKey}); //tell the content script that a valid api key was entered
+      });
     } else {
       alert('Looks like you entered an invalid API key! Get one from the link.');
     }
-    chrome.tabs.query(params, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {message: "keyIn",key:apiKey});
-    });
   }
 });
