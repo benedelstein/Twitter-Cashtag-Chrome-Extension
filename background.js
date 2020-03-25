0ef467f7a96f538f8b17e2a63b74ade8163ed4be
Ben Edelstein <bedelstein12@gmail.com>
2020-03-24 22:40:21 -0400

    aaa

--- background.js
+++ background.js
@@ -2,8 +2,8 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) { //wai
   if(details.url.includes('twitter.com/search?q=%24')) {
     console.log(details.transitionType);
     chrome.tabs.executeScript(null,{file:"jquery-3.4.1.min.js"}); // reload jquery first
-    chrome.tabs.executeScript(null,{file:"plotly-basic.min.js"});
-    chrome.tabs.executeScript(null,{file:"stocks.js"});
-    chrome.tabs.executeScript(null,{file:"content.js"}); //re-run content script
+    chrome.tabs.executeScript(null,{file:"plotly-basic.min.js"}); //then plotly
+    chrome.tabs.executeScript(null,{file:"stocks.js"}); //then stocks
+    chrome.tabs.executeScript(null,{file:"content.js"}); //finally re-run content script
   }
 });
