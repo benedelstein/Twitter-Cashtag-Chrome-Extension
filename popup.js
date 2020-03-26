32961c12c522af264f0169e19c7531db8139e4da
Ben Edelstein <bedelstein12@gmail.com>
2020-03-25 21:40:14 -0400

    style popup

--- popup.js
+++ popup.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  $('#sendKey').on("click", onKeySubmit); //listen for button press
+  $('#submitButton').on("click", onKeySubmit); //listen for button press
   chrome.storage.sync.get(['apiKey'], function(result) {
     var keyData;
     if(result.apiKey) {
-      keyData = 'current API key: ' + result.apiKey;
+      keyData = 'your current API key: ' + result.apiKey;
     } else {
-      keyData = 'You haven\'t entered an API key yet.';
+      keyData = 'You haven\'t entered an API key yet';
     }
     $('#currentKey').html(keyData);
   });
@@ -20,6 +20,8 @@ $(document).ready(function() {
       /*TODO: check if API KEY is actually valid*/
       chrome.storage.sync.set({apiKey: apiKey},function() {
         console.log('Set user api key to ' + apiKey);
+        var newKeyData = 'your current API key: ' + apiKey;
+        $('#currentKey').html(newKeyData);
       });
       chrome.tabs.query(params, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {message: "keyIn",key:apiKey}); //tell the content script that a valid api key was entered
