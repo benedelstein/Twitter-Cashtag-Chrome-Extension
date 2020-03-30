2510d03e71f62bbbbdfa263700869539bd028468
Ben Edelstein <bedelstein12@gmail.com>
2020-03-29 21:18:07 -0400

    confirmation page for key input

--- popup.js
+++ popup.js
@@ -1,14 +1,28 @@
 $(document).ready(function() {
-  $('#submitButton').on("click", onKeySubmit); //listen for button press
   chrome.storage.sync.get(['apiKey'], function(result) {
     var keyData;
     if(result.apiKey) {
       keyData = 'your current API key: ' + result.apiKey;
+      $('#noKeyYet').hide();
+      $('#apiKey').text(result.apiKey);
     } else {
       keyData = 'You haven\'t entered an API key yet';
+      $('#keyIn').hide();
     }
-    $('#currentKey').html(keyData);
+    $('#noKeyMsg').html(keyData);
   });
+
+  $('#submitButton').on("click", onKeySubmit); //listen for key entry
+  $('#keyInput').on('keypress',function(e) {
+    if(e.which == 13) {
+        onKeySubmit();
+    }
+  });
+
+  $('#reenter').on("click", function() {
+    $('#keyIn').hide();
+    $('#noKeyYet').show();
+  }); //listen for button press
 
   function onKeySubmit() { //function run when submit button is pressed
     let params = {
@@ -17,11 +31,15 @@ $(document).ready(function() {
     }
     let apiKey = $('#keyInput').val().trim();
     if(apiKey.length===16) { // if key is valid
-      /*TODO: check if API KEY is actually valid*/
+      /* TODO: check if API KEY is actually valid */
       chrome.storage.sync.set({apiKey: apiKey},function() {
         var newKeyData = 'your current API key: ' + apiKey;
-        $('#currentKey').html(newKeyData);
+        $('#noKeyMsg').html(newKeyData);
+        $('#apiKey').text(apiKey);
       });
+      $('#noKeyYet').hide();
+      $('#keyIn').show();
+      $('#keyInput').val('');
       chrome.tabs.query(params, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {message: "keyIn",key:apiKey}); //tell the content script that a valid api key was entered
       });
